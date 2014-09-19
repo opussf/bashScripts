39b84d150e683768a0084c50a044c609e32c4adb
Opus <opus@zz9-za.com>
2014-09-19 15:32:44 -0700

    Cleaner running script.
    Since gitupdate has the repo as the CWD, put the log file in the parent dir.
    Less output to stdout during run, and cleaner output in the log file.

--- gitSupport/update.sh
+++ gitSupport/update.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
+#########################################
+## This script looks for git repos as subdirectories and fetches remote updates.
+## The updates are performed in parallel, with update info being logged,
+## and reported on at the end.
 
 gitupdate() {
-	echo "========" > update.log
-	echo "Updating ${1}" >> update.log
-	echo "--------" >> update.log
-	git remote -v update >> update.log 2>&1
-	git pull --ff-only >> update.log 2>&1
-	echo "========" >> update.log
+	echo "Updating: -- ${1} --" >> ../update.${1}.log
+	echo "--------" >> ../update.${1}.log
+	git remote -v update >> ../update.${1}.log 2>&1
+	git pull --ff-only >> ../update.${1}.log 2>&1
+	echo "========" >> ../update.${1}.log
 }
 
+echo "=========="
 for repo in `ls`
 do
 if [ -d $repo ]; then
-echo "=========="
-echo "    ${repo}"
 pushd $repo >> /dev/null
 if [ -d ".git" ]; then
+	echo "(git) ${repo}"
 	gitupdate $repo &
 fi
 popd >> /dev/null
 fi
 done
+echo "--- Waiting for updates to happen ---"
 wait
 echo "=========="
 rm update.log
 for repo in `ls`
 do
-if [ -f ${repo}/update.log ]; then
-	cat ${repo}/update.log >> update.log
+if [ -f update.${repo}.log ]; then
+	cat update.${repo}.log >> update.log
+	rm update.${repo}.log
 fi
 done
 cat update.log
